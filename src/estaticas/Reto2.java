912e348481c42b8ac07d8c8de6cc8b4b36ef95db
JuanSusa <kmilosusasusa@gmail.com>
2022-10-30 20:05:29 -0500

    se termina Reto2 arrays

---
+++ src/estaticas/Reto2.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estaticas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jennifer
+ */
+public class Reto2 {
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        int competidores;
+        
+
+        System.out.println("Digite la cantidad de competidores a participar ");
+        competidores = leer.nextInt();
+
+        String[] nombre = new String[competidores];
+        int[] tiempo = new int[competidores];
+        for (int i = 0; i < nombre.length; i++) {
+            System.out.println("Ingrese el nombre del participante " + i);
+            nombre[i] = leer.next();
+        }
+        System.out.println("En total habrá un total de " + competidores + " participantes en la competencia de natación.");
+
+        for (int t = 0; t < tiempo.length; t++) {
+
+            System.out.println("Ingrese el tiempo del participante " + t);
+            tiempo[t] = leer.nextInt();
+        }
+        int tiempoMayor = 0;
+        for (int x = 0; x < tiempo.length; x++) {
+            
+            if (tiempo[x] > tiempoMayor) {
+
+                tiempoMayor = tiempo[x];
+            }
+        }
+        System.out.println("el ganador es" + nombre[i] + "tiempo mayor es de " + tiempoMayor);
+    }
+}
